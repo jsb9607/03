9a3731f7dcd7a5e0131bed3bacac2daf4d6f1af4
jsb9607 <57170950+jsb9607@users.noreply.github.com>
2020-09-17 16:42:18 +0900

    last

--- main.c
+++ main.c
@@ -5,14 +5,13 @@
 
 int main(int argc, char *argv[]) {
 
-	char a;
-	char b;
+	int a, b;
+	double c;
 
-	printf("enter a character : ");
-	scanf("%c", &a);
-	
-	b = a + 1;
-	printf("the  next character of %c(%i) is %c(%i)\n", a, a, b, b);
+	printf("input two integers : ");
+	scanf("%d %d", &a, &b);
+
+	printf("result is %f\n ", (float)a/b);
 	
 	return 0;
 }
